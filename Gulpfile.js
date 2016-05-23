f5d270c49e186905e2d5bd7331e12d943a7d751a
Nic <channicoleelise@gmail.com>
2016-05-23 20:05:47 +0800

    Set up variables and completed header

---
+++ Gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var autoprefixer = require ('gulp-autoprefixer');
+
+var sassInput = ['scss/**/*.scss'];
+
+gulp.task('sass', function () {
+  return gulp
+    .src(sassInput)
+    .pipe(sass().on('error', swallowError))
+    .pipe(autoprefixer())
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+//Watch task
+gulp.task('default',function() {
+    gulp.watch('scss/**/*.scss',['sass']);
+});
+
+function swallowError (error) {
+  console.log(error.toString())
+  this.emit('end')
+}    
